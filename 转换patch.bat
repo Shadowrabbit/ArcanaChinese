73601f7914bb2e797f9d641ef13922abe9285eca
BILIBILI\wangyue07 <wangyue07@bilibili.com>
2021-02-18 21:52:01 +0800

    object

--- 转换patch.bat
+++ 转换patch.bat
@@ -1,4 +1,4 @@
 @echo off
-for /f "tokens=* delims=" %%i in ('dir /b/s *.matitem') do (
-rename "%%i" "*.matitem.patch"
+for /f "tokens=* delims=" %%i in ('dir /b/s *.object') do (
+rename "%%i" "*.object.patch"
 )
